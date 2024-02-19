5eddf7bbc066542734cad4d796a48dc97469dfce
Hys-Lee <dltkdgus3275@gmail.com>
2024-02-19 16:26:27 +0900

    meta추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          property="og:url"
+          content="https://next-tutorial-bay-one.vercel.app/"
+        />
+        <meta property="og:title" content="넥스트 튜토리얼" />
+        <meta property="og:description" content="설명설명" />
+        <meta property="og:type" content="website" />
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
